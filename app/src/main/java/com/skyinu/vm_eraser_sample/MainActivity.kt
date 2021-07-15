f3fe3c13c8f61119a5cb5c1ba6d45db9809d4c4f
skyinu <haochenu@gmail.com>
2021-07-15 23:00:16 +0800

    feat:try to use compose

--- app/src/main/java/com/skyinu/vm_eraser_sample/MainActivity.kt
+++ app/src/main/java/com/skyinu/vm_eraser_sample/MainActivity.kt
@@ -4,21 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.compose.setContent
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import com.skyinu.vm_eraser.MemoryFileParser
 import com.skyinu.vm_eraser.VMEraser
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            buildView()
+        }
         VMEraser.init()
         VMEraser.eraser("/system/fonts/(?!Roboto).*\\.ttf")
-        findViewById<View>(R.id.hello).setOnClickListener {
+    }
+
+    @Composable
+    private fun buildView() {
+        Button(onClick = {
             MemoryFileParser.parse().forEach {
                 if (it.name.contains("ttf")) {
                     Log.e("VMEraser", it.toString())
                 }
             }
+        }) {
+            Text("print ttf memory info")
         }
     }
 }
