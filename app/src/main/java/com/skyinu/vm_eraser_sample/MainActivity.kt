50dacd2259032a54fb9af309ba40ba63f24193a4
skyinu <haochenu@gmail.com>
2021-09-23 22:23:29 +0800

    feat:add test data

--- app/src/main/java/com/skyinu/vm_eraser_sample/MainActivity.kt
+++ app/src/main/java/com/skyinu/vm_eraser_sample/MainActivity.kt
@@ -2,9 +2,11 @@ package com.skyinu.vm_eraser_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Debug
 import android.util.Log
 import android.view.View
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,25 +14,37 @@ import com.skyinu.vm_eraser.MemoryFileParser
 import com.skyinu.vm_eraser.VMEraser
 
 class MainActivity : AppCompatActivity() {
+    private var vmSizeBefore = 0L
+    private var nativeHeapSize = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             buildView()
         }
         VMEraser.init()
-        VMEraser.eraser("/system/fonts/(?!Roboto).*\\.ttf")
     }
 
     @Composable
     private fun buildView() {
-        Button(onClick = {
-            MemoryFileParser.parse().forEach {
-                if (it.name.contains("ttf")) {
-                    Log.e("VMEraser", it.toString())
-                }
+        Column {
+            Button(onClick = {
+                vmSizeBefore = MemoryUtils.getVMSize()
+                nativeHeapSize = Debug.getNativeHeapSize()
+                VMEraser.eraser("/system/fonts/(?!Roboto).*\\.ttf")
+                setContent { buildView() }
+            }) {
+                Text("click to free vm")
             }
-        }) {
-            Text("print ttf memory info")
+            Text(
+                "vm change: before = $vmSizeBefore kB, " +
+                        "now ${MemoryUtils.getVMSize()} kB, " +
+                        "gap = ${vmSizeBefore - MemoryUtils.getVMSize()}kB"
+            )
+            Text(
+                "native heap change: before = $nativeHeapSize B, " +
+                        "now ${Debug.getNativeHeapSize()} B, " +
+                        "gap = ${nativeHeapSize - Debug.getNativeHeapSize()} B"
+            )
         }
     }
 }
---
+++ app/src/main/java/com/skyinu/vm_eraser_sample/MemoryUtils.kt
@@ -0,0 +1,25 @@
+package com.skyinu.vm_eraser_sample
+
+import android.os.Process
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStreamReader
+
+object MemoryUtils {
+    //单位kB
+    fun getVMSize(): Long {
+        val mmapFile = File(getStatusFilePath())
+        val bufferedReader = BufferedReader(InputStreamReader(FileInputStream(mmapFile)))
+        bufferedReader.lineSequence().iterator().forEach {
+            if (it.startsWith("VmSize")) {
+                return it.split(" ")[1].toLong()
+            }
+        }
+        return 0
+    }
+
+    private fun getStatusFilePath(): String {
+        return "proc" + File.separator + Process.myPid() + File.separator + "status"
+    }
+}
